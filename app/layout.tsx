91cc13e62b836094eb477bebbe79675f574f19f6
vkgolas <vkgolas>
2024-01-27 14:53:53 +0530

    add header and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Layout/header";
+import Footer from "./components/Layout/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Header />
+      {children}
+      <Footer />
     </html>
   );
 }
