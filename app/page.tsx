31c72ea511ccb09537855c71b98f791b5ebb0b3b
Sushil0011 <yashtripathi861@gmail.com>
2024-01-27 15:07:26 +0530

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,9 @@
 import Image from "next/image";
 import Products from "./components/products";
-
-async function getData() {
-  const res = await fetch("https://api.escuelajs.co/api/v1/products");
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
+import { getData } from "@/utils/getData";
 
 export default async function Home() {
-  const data: any = await getData();
-
+  const data: any = await getData("https://api.escuelajs.co/api/v1/products");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 border border-red-500">
       <Products data={data as any} />
